8b581610b21b57480fca45e2765754993129bcbe
diogoguedes <diogo.pereira@4all.com>
2019-09-29 10:36:24 -0300

    ajuste

---
+++ build/precache-manifest.f6b51517eb10444a0f43f07bab203e0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca3d300bd53d49c69367f1ed284f147",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cf113d86e4badb5bab3",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d9fbb4da9c2a1377ac76",
+    "url": "/static/js/2.53b1048d.chunk.js"
+  },
+  {
+    "revision": "5cf113d86e4badb5bab3",
+    "url": "/static/js/main.19084866.chunk.js"
+  },
+  {
+    "revision": "85f9883f38bd38954cf7",
+    "url": "/static/js/runtime-main.c81c18fa.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
